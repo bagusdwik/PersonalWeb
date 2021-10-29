1530f81a56fe9feecbb5a39bbd7dc9a69e4d81d4
Bagus Dwi K <77946920+Bagusdwik@users.noreply.github.com>
2021-10-29 09:35:38 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	$(window).scroll(function() {
+		if (this.scrollY > 20) {
+			$(".navbar").addClass("sticky");
+			$(".gotop").fadeIn();
+		}
+		else{
+			$(".navbar").removeClass("sticky");
+			$(".gotop").fadeOut();
+		}
+	})
+
+	$(".gotop").click(function(){scroll(0,0)});
+
+	$('.menu-toggler').click(function() {
+		$(this).toggleClass("active");
+		$(".navbar-menu").toggleClass("active");
+	});
+
+	$(".Works").magnificPopup({
+		delegate: 'a', 
+		type: 'image', 
+		gallery: {enabled:true}
+	});
+})
